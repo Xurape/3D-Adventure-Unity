77a1966f83900a31b62d1701fdd342744663715e
João Ferreira <j.pedro.ferreira@ipvc.pt>
2024-04-19 21:39:00 +0100

    add: gamekit

---
+++ Assets/Samples/GameKit/3DGamekit/Scripts/Game/Core/FPSTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Gamekit3D
+{
+    public class FPSTarget : MonoBehaviour
+    {
+        public int targetFPS = 120;
+
+        void OnEnable()
+        {
+            SetTargetFPS(targetFPS);
+        }
+
+        public void SetTargetFPS(int fps)
+        {
+            Application.targetFrameRate = fps;
+        }
+    }    
+}
